79f362ca52d13debf5d203316bbf07a3b16fa554
Claudio Castello <claudio.chemist@gmail.com>
2021-03-10 11:47:21 -0300

    Created secrets.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import secrets
+
+consumer_key = secrets.TWITTER_CONSUMER_KEY
+consumer_secret = secrets.TWITTER_CONSUMER_SECRET
+access_token = secrets.TWITTER_ACCESS_TOKEN
+access_secret = secrets.TWITTER_ACCESS_SECRET
--- mining_tt.py
+++ mining_tt.py
@@ -1,13 +1,14 @@
-import tweepy, tt_auth
+import tweepy, config
 from tweepy import OAuthHandler
  
-consumer_key = tt_auth.c_key
-consumer_secret = tt_auth.c_secret
-access_token = tt_auth.a_token
-access_secret = tt_auth.a_secret
+consumer_key = config.consumer_key
+consumer_secret = config.consumer_secret
+access_token = config.access_token
+access_secret = config.access_secret
 
-
-# auth = OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_secret)
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
  
-# api = tweepy.API(auth)
+api = tweepy.API(auth)
+
+status = tweepy.Cursor(api.home_timeline).items(10)
